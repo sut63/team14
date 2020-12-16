fd3eeda9c1d560397b0cc1e27d9642f8bd9f1b24
tew147258 <tew147258@gmail.com>
2020-12-16 14:29:00 +0700

    ทำ Entity ของตัวติดตามbug - close #30

---
+++ backend/ent/schema/adminrepair.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Adminrepair holds the schema definition for the Adminrepair entity.
+type Adminrepair struct {
+	ent.Schema
+}
+
+// Fields of the Adminrepair.
+func (Adminrepair) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Adminrepair.
+func (Adminrepair) Edges() []ent.Edge {
+	return nil
+}
